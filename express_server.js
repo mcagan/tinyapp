a8779daca2e2bee5feca8cbc74dc2c8d4e7918a3
mcagan <meliscagan@gmail.com>
2020-03-18 00:44:56 +0000

    add login and logout functionality

--- express_server.js
+++ express_server.js
@@ -2,9 +2,12 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser');
+
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs")
+app.use(cookieParser());
 
 function generateRandomString() {
   let result = '';
@@ -25,38 +28,49 @@ const updateURL = (shortID, url) => {
   urlDatabase[shortID] = url;
   return true;
 }
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
 
 app.get("/", (req, res) => {
-  res.redirect("urls_show");
+  res.redirect("/urls");
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
+// app.get("/urls.json", (req, res) => {
+//   res.json(urlDatabase);
+// });
 
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  let templateVars = { urls: urlDatabase, username: req.cookies["username"] };
   res.render("urls_index", templateVars);
 });
 
+app.post("/login", (req, res) => {
+  let username = req.body.username;
+  res.cookie("username", username);
+  res.redirect("/urls");
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
+  res.redirect("/urls");
+});
+
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  templateVars = { username: req.cookies["username"] }
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies["username"] };
   res.render("urls_show", templateVars);
-});
-
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
 });
 
 app.post("/urls", (req, res) => {
   console.log(req.body);  // Log the POST request body to the console
   shortURL = generateRandomString();
   urlDatabase[shortURL] = req.body.longURL;
-  templateVars = {shortURL: shortURL, longURL: req.body};
+  templateVars = {shortURL: shortURL, longURL: req.body, username: req.cookies["username"]};
   res.render("urls_show", templateVars);
 });
 
